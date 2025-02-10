7611c6cd99f32fabebc475dc7e32c7fbb7e075b6
plvkyan <kyanacademics@gmail.com>
2025-02-10 18:45:07 +0800

    feat(Home.tsx): add navigation bar to home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,11 +3,16 @@
 // Component imports
 // Button component import
 import { Button } from "@/components/ui/button"
+// Navigation bar component import
+import NavigationBar from "@/components/ui/navigation-bar"
+
+
 
 const Home = () => {
 
     return (
         <div>
+            <NavigationBar />
             <h1> CEIT Student-Athlete Shines with Dual Gold Medals in the PLV 2022 Intramurals </h1>
             <p> With her combination of athleticism, beauty, and intelligence, 
                 April Joy Yapcengco is not only a campus crush but also a 
