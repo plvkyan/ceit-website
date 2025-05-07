5a5b9c02bb0ab27a056535687ba5e1a2b5f51282
plvkyan <kyanacademics@gmail.com>
2025-05-07 10:59:19 +0800

    style(carousel.tsx): change carousel dots style

--- client/src/components/ui/carousel.tsx
+++ client/src/components/ui/carousel.tsx
@@ -282,7 +282,7 @@ const CarouselDots = React.forwardRef<
         {Array.from({ length: numberOfSlides }, (_, i) => (
           <Button
             key={i}
-            className={`mx-1 h-2 w-2 rounded-full p-0 ${
+            className={`mx-1 h-1.5 w-1.5 lg:h-2 lg:w-2 rounded-full p-0 ${
               i === currentSlide
                 ? 'scale-125 transform bg-primary'
                 : 'bg-gray-300 hover:bg-gray-400'
